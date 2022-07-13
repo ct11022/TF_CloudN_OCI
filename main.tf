d97d77eeab01ca8bc8f6f1608385e2abbd9de858
C.Tseng <ctseng@aviatrix.com>
2022-07-13 22:03:38 +0800

    change source path and make controller can ssh login

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ module "aviatrix_controller_build_new_vpc" {
 #Buile Aviatrix controller at existed VPC
 module "aviatrix_controller_build_existed_vpc" {
   count   = (local.new_vpc ? 0 : 1)
-  source  = "github.com/AviatrixSystems/terraform-modules.git//aviatrix-controller-build?ref=master"
+  source  = "git@github.com:AviatrixDev/terraform-modules-aws-internal.git//aviatrix-controller-build?ref=main"
   vpc     = var.controller_vpc_id
   subnet  = var.controller_subnet_id
   keypair = (local.new_key ? aws_key_pair.controller[0].key_name : var.keypair_name)
@@ -86,7 +86,7 @@ module "aviatrix_controller_build_existed_vpc" {
   name_prefix = var.testbed_name
   root_volume_size = "64"
   incoming_ssl_cidr = "${concat(var.incoming_ssl_cidr, [var.controller_vpc_cidr])}"
-
+  ssh_cidrs = var.incoming_ssl_cidr
 }
 
 resource "time_sleep" "wait_210s" {
@@ -254,7 +254,6 @@ data "oci_core_internet_gateways" "tr_internet_gateway" {
   count          = (local.pure_oci_tr_vcn_id != null ? 1 : 0)
   compartment_id = var.oci_compartment_id
   vcn_id         = local.pure_oci_tr_vcn_id
-  # vcn_id         = aviatrix_transit_gateway.transit.vpc_id
 }
 data "oci_core_subnets" "tr_subnet" {
     count          = (local.pure_oci_tr_vcn_id != null ? 1 : 0)
